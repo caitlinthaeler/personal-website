e358b3614bd83390a5d2eec5142ed288b597f4f5
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2025-01-13 23:10:15 +0000

    composition API short form

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,20 @@
-<script>
+<script setup>
 import { ref } from 'vue';
 
-export default {
-  setup() {
-    const name = ref('kristin thaeler');
-    const status = ref('active');
-    const tasks = ref(['task 1', 'task 2', 'task 3']);
+const name = ref('kristin thaeler');
+const status = ref('active');
+const tasks = ref(['task 1', 'task 2', 'task 3']);
 
-    const toggleStatus = () => {
-      if (status.value === 'active') {
-        status.value = 'pending';
-      } else if (status.value === 'pending') {
-        status.value = 'inactive';
-      } else {
-        status.value = 'active';
-      }
-    }
-
-    return {
-      name,
-      status,
-      tasks,
-      toggleStatus,
-    }
-  },
+const toggleStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending';
+  } else if (status.value === 'pending') {
+    status.value = 'inactive';
+  } else {
+    status.value = 'active';
+  }
 };
+
 </script>
 
 
