e3551fecb7956a4900dae35a7700005d3cf9bb48
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2025-01-14 22:26:39 +0000

    job listings

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,7 +2,7 @@
 import Navbar from '@/components/Navbar.vue'
 import Hero from '@/components/Hero.vue'
 import HomeCards from '@/components/HomeCards.vue'
-
+import JobListings from '@/components/JobListings.vue'
 </script>
 
 <template>
@@ -10,5 +10,6 @@ import HomeCards from '@/components/HomeCards.vue'
     <Navbar />
     <Hero/>
     <HomeCards />
+    <JobListings :limit="3" :showButton="true"/>
   </div>
 </template>
