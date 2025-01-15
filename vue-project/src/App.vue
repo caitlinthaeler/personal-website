e330490b5a51e59c6b8fd51111e8319011a4831a
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2025-01-15 04:53:03 +0000

    router and jobs view

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
-import Hero from '@/components/Hero.vue'
-import HomeCards from '@/components/HomeCards.vue'
-import JobListings from '@/components/JobListings.vue'
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div class="font-handwriting text-2xl">
     <Navbar />
-    <Hero/>
-    <HomeCards />
-    <JobListings :limit="3" :showButton="true"/>
+    <RouterView />
   </div>
 </template>
