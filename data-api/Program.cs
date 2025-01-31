1e0f541c0bfcae618159c0c1d056bbd91139002e
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2025-01-31 16:49:16 +0000

    changed from .net 9 to .net 8

--- data-api/Program.cs
+++ data-api/Program.cs
@@ -62,18 +62,28 @@ builder.Services.AddControllers();
 
 //builder.Services.AddScoped<IDataService<Project>, MongoDBService<Project>>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "data_api",
+        Version = "v1"
+    });
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.MapStaticAssets();
+//app.MapStaticAssets();
 
 app.UseCors(AllowOriginsPolicyName);
 
