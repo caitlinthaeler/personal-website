dcb9230f0e2da844c874c702cb5515629450172a
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2025-02-02 06:54:36 +0000

    changed http to https

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5283',
+        target: 'https://localhost:5283',
         changeOrigin: true,
         secure: false,
         //rewrite: (path) => path.replace(/^\/api/, ''),
