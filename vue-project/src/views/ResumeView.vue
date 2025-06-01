ea914cf3927746d725b1d48832d46fdd803b7ac5
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2025-06-01 17:10:51 +0100

    updated resume

--- vue-project/src/views/ResumeView.vue
+++ vue-project/src/views/ResumeView.vue
@@ -1,9 +1,49 @@
 <script setup>
 import { RouterLink } from 'vue-router'
+import { ref, computed } from 'vue'
+
+const props = defineProps({
+    frameWidth: {
+        type: Number,
+        default: 1024
+    },
+    frameHeight: {
+        type: Number,
+        default: 576
+    },
+    scale: {
+        type: Number,
+        default: 1
+    },
+    resumeUrl: {
+        type: String,
+        default: '@/assets/img/caitlin-thaeler-cv-2025-5.pdf'
+    }
+});
+
+const resume = computed(() => {
+  // Handle both local and external URLs
+  if (props.resumeUrl.startsWith('@/')) {
+    // Convert alias to relative path
+    const path = props.resumeUrl.replace('@/', '../');
+    return new URL(path, import.meta.url).href;
+  } else {
+    return props.resumeUrl;
+  }
+});
+
+function downloadResume() {
+  const link = document.createElement('a');
+  link.href = resume.value;
+  link.download = 'caitlin-thaeler-cv.pdf'; // or any custom name
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 </script>
 
 <template>
-    <div>
+    <div class="bg-grape h-full">
         <div class="p-10">
             <h1 class="text-grape-light text-7xl">
                 Resume
@@ -11,17 +51,17 @@ import { RouterLink } from 'vue-router'
             
             <div class="flex flex-row justify-between pb-2">
                 <p class="mt-10 text-grape-light text-md">
-                Updated in January 2025
+                Updated in May 2025
                 </p>
-                <button class="text-grape bg-lemon hover:bg-plum rounded-md px-3 py-2 mt-4">
+                <button @click="downloadResume" class="text-grape bg-lemon hover:bg-plum rounded-md px-3 py-2 mt-4">
                     Download Resume
                 </button>
             </div>
             <div class="bg-lemon p-5">
-                <p >
-                    Resume
-                </p>
-                <p class="p-5 text-center">
+                <div v-if="resume">
+                    <iframe :src="resume" width="100%" height="700px"></iframe>
+                </div>
+                <p v-else class="p-5 text-center">
                     Nothing here yet
                 </p>
             </div>
